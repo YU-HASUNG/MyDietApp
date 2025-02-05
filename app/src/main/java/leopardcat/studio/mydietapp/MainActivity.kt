1662c8f0343efd86da6e4487ee9d07560b7331c9
YU-HASUNG <harry.yu@kyobobook.com>
2025-02-05 22:59:15 +0900

    [250205] 하단 네비게이션 작업 - 하단 네비게이션 생성 - 네비게이션별 탭 생성

--- app/src/main/java/leopardcat/studio/mydietapp/MainActivity.kt
+++ app/src/main/java/leopardcat/studio/mydietapp/MainActivity.kt
@@ -3,13 +3,24 @@ package leopardcat.studio.mydietapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import leopardcat.studio.mydietapp.ui.main.DietAppScreen
+import leopardcat.studio.mydietapp.ui.main.MainNavigation
+import leopardcat.studio.mydietapp.ui.main.screens.DietScreen
+import leopardcat.studio.mydietapp.ui.main.screens.HomeScreen
+import leopardcat.studio.mydietapp.ui.main.screens.ProfileScreen
 import leopardcat.studio.mydietapp.ui.theme.MyDietAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +28,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyDietAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+
+                var currentScreen by remember{ mutableStateOf(DietAppScreen.Home) }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black)
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier
+                            .padding(bottom = 60.dp)
+                            .fillMaxSize()
+                    ) {
+                        when(currentScreen) {
+                            DietAppScreen.Home -> HomeScreen()
+                            DietAppScreen.Diet -> DietScreen()
+                            DietAppScreen.Profile -> ProfileScreen()
+                        }
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    MainNavigation(currentScreen = currentScreen, onTabSelected = {
+                        currentScreen = it
+                    })
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyDietAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/leopardcat/studio/mydietapp/ui/main/MainNavigation.kt
@@ -0,0 +1,88 @@
+package leopardcat.studio.mydietapp.ui.main
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import leopardcat.studio.mydietapp.R
+
+@Composable
+fun MainNavigation(
+    currentScreen: DietAppScreen,
+    onTabSelected: (DietAppScreen) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .background(Color.Black),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        DietAppScreen.entries.forEach { screen ->
+
+            val isSelected = currentScreen == screen
+
+            val color by animateColorAsState(
+                targetValue = if (isSelected) Color.White else Color.Gray,
+                animationSpec = spring(), label = ""
+            )
+
+            val scale by animateFloatAsState(
+                targetValue = if (isSelected) 1.2f else 1.0f,
+                animationSpec = spring(), label = ""
+            )
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .clickable { onTabSelected(screen) }
+                    .scale(scale)
+                    .padding(3.dp)
+            ) {
+
+                Icon(
+                    painter = painterResource(id = screen.icon),
+                    contentDescription = screen.name,
+                    tint = color
+                )
+
+                Text(
+                    text = screen.name,
+                    color = color
+                )
+
+            }
+        }
+    }
+}
+
+enum class DietAppScreen(val icon: Int) {
+    Home(R.drawable.tab1_icon),
+    Diet(R.drawable.tab2_icon),
+    Profile(R.drawable.tab3_icon)
+}
+
+@Preview
+@Composable
+fun MainNavigationPreview() {
+    MainNavigation(DietAppScreen.Home) { }
+}
